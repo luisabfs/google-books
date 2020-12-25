f0fa8389badee514bb3eba737292512077a1718c
Luísa Barros <qlullyq@gmail.com>
2020-12-25 20:52:11 -0300

    feat: create form for book searching and list data from books api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import booksAPI from './services/books';
 
-function App() {
+const API_KEY = 'AIzaSyClUw246sxrG1f-qgBGLwkeD2EU8EQAu9E';
+
+const App = () => {
+  const inputRef = useRef(null);
+  const [books, setBooks] = useState([]);
+
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+
+      const { data } = await booksAPI.get(
+        `/volumes?q=${inputRef.current.value}&key=${API_KEY}`,
+      );
+
+      setBooks(data.items);
+    },
+    [inputRef],
+  );
+
   return (
-    <div className="App">
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef} />
+        <button type="submit">Pesquisar</button>
+      </form>
+
+      <ul>
+        {books.map((book) => (
+          <li>{book.volumeInfo.title}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
